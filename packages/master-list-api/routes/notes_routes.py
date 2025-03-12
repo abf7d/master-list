6da580f48aa4f1086cc2b66cf9f12f0f167d7d80
afriedman111 <afriedman111@gmail.com>
2025-03-12 07:17:17 -0400

    width style for editor, print for claims

--- packages/master-list-api/routes/notes_routes.py
+++ packages/master-list-api/routes/notes_routes.py
@@ -14,6 +14,7 @@ from core.auth import authenticate
 from sqlalchemy.orm import Session
 from typing import List
 import logging
+from services.graph_service import GraphService
 
 router = APIRouter()
 
@@ -26,14 +27,21 @@ logger = logging.getLogger("routers.bins")
 def get_note_service(db: Session = Depends(get_db)):
     return NoteService(db)
 
+def get_graph_service():
+    return GraphService()
+
 @router.get("/tags/", response_model=List[TagResponse])
 @authenticate
 async def get_tags(
     request: Request,
+    graph_service: GraphService = Depends(get_graph_service),
     note_service: NoteService = Depends(get_note_service),
 ):
     """Create a new tag"""
     print('USERID!!!!!!!!! ', request.state.user_id)
+    claims = await graph_service.get_claims(request.state.user_id)
+    print('CLAIMS!!!!!!!!! ', claims)
+    
     return note_service.get_tags(parent_tag_id=None)
 @router.get("/tags/{parent_tag_id}/children", response_model=List[TagResponse])
 async def get_child_tags(
